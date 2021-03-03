c92a046cdbd431c4010fa3f402f454296abae2ba
Smertin Nikita <smertin-nikita@mail.ru>
2021-03-03 15:45:20 +0500

    realized stack class

--- stack.py
+++ stack.py
@@ -1,3 +1,18 @@
 
-if __name__ == '__main__':
-    pass
+
+class Stack(list):
+
+    def is_empty(self):
+        return not self
+
+    def push(self, item):
+        super().append(item)
+
+    def pop(self):
+        return super().pop()
+
+    def peek(self):
+        return self[len(self) - 1]
+
+    def size(self):
+        return len(self)
